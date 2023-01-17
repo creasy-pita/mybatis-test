edcddffb6542967f40f2b78479cda59789bc4265
creasy-pita <hz_ljq@live.com>
2023-01-17 13:29:19 +0800

    1 数据库连接修改

--- src/main/java/xyz/coolblog/mybatis/ArticleTypeHandler.java
+++ src/main/java/xyz/coolblog/mybatis/ArticleTypeHandler.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.MappedTypes;
 import xyz.coolblog.constant.ArticleTypeEnum;
-import xyz.coolblog.model.Article;
+import xyz.coolblog.chapter4.model.Article;
 
 /**
  * ArticleTypeHandler
